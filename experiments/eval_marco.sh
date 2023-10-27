ef3c3ecccbe7cc3ffa407153279fe147159f8939
Hak_Han <hanpx20@mails.tsinghua.edu.cn>
2023-10-27 14:40:45 +0800

    $

--- experiments/eval_marco.sh
+++ experiments/eval_marco.sh
@@ -1,21 +1,5 @@
 #Remember to Modify variables according to your need
 
-torchrun --nproc_per_node=8 --master_port 19286 -m openmatch.driver.train_dr  \
-    --output_dir $CHECKPOINT_DIR/Anchor_DR/t5  \
-    --model_name_or_path $PLM_DIR/t5-ckpt  \
-    --do_train  \
-    --save_steps 4000  \
-    --train_path /home/hanpeixuan/utilities/processed_data/Anchor_DR/train.jsonl  \
-    --fp16  \
-    --per_device_train_batch_size 24  \
-    --train_n_passages 8  \
-    --learning_rate 1e-5  \
-    --q_max_len 32  \
-    --p_max_len 128  \
-    --num_train_epochs 1  \
-    --logging_dir $LOG_DIR/Anchor_DR/t5 \
-    --negatives_x_device \
-    --wandb_id AnchorDR
 
 torchrun --nproc_per_node=8 --master_port 19286 -m openmatch.driver.build_index  \
     --output_dir $EMBEDDING_DIR/msmarco/Anchor_DR  \
